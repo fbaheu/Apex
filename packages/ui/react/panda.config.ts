470ba15d076849e6a11f7cec80cc7648a5e769a9
Florian Baheu <florian.baheu-isc_france@fr.ibm.com>
2025-10-01 15:52:26 +0200

    chore: change most package.json scripts

--- packages/ui/react/panda.config.ts
+++ packages/ui/react/panda.config.ts
@@ -2,25 +2,25 @@ import { defineConfig } from '@pandacss/dev';
 import { createPreset } from '@apex/preset-ui';
 
 export default defineConfig({
-	// Whether to use css reset
-	preflight: true,
+  // Whether to use css reset
+  preflight: true,
 
-	presets: [createPreset()],
+  presets: [createPreset()],
 
-	// Where to look for css declarations
-	include: [
-		'./src/**/*.{ts,tsx}',
-	],
+  // Where to look for css declarations
+  include: [
+    './src/**/*.{ts,tsx}',
+  ],
 
-	// Files to exclude
-	exclude: ['node_modules'],
+  // Files to exclude
+  exclude: ['node_modules'],
 
-	// Import mapping
-	importMap: '@apex/design-system',
+  // Import mapping
+  importMap: '@apex/design-system',
 
-	// The JSX framework to use
-	jsxFramework: 'react',
+  // The JSX framework to use
+  jsxFramework: 'react',
 
-	// The output directory for your css system (monorepo config)
-	outdir: '../design-system',
+  // The output directory for your css system (monorepo config)
+  outdir: '../design-system',
 });
